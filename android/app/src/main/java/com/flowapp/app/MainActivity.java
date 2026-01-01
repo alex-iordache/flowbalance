e847570a474764a11e77658513245a87fefbaac9
Alexandru Iordache <alexandru.iordache@mrm.com>
2026-01-01 21:40:37 +0200

    Enable Android WebView remote debugging in debug builds

--- android/app/src/main/java/com/flowapp/app/MainActivity.java
+++ android/app/src/main/java/com/flowapp/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.flowapp.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Enable remote debugging (chrome://inspect) for Android WebView in debug builds.
+    if (BuildConfig.DEBUG) {
+      WebView.setWebContentsDebuggingEnabled(true);
+    }
+  }
+}

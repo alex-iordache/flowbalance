f8ad6a9bd0b1dd9c85e2cadaade3107a7df07526
Alexandru Iordache <alexandru.iordache@mrm.com>
2026-03-02 12:06:56 +0200

    bugfix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ module.exports = {
   // `next dev` and `next build` both write into `distDir`.
   // When running a build while the dev server is running, they can stomp on each other.
   // We allow overriding the build output directory so we can run "isolated" builds.
-  distDir: process.env.NEXT_DIST_DIR || '.next',
+  // However, on Vercel we should always stick to the default `.next` folder.
+  // A misconfigured `NEXT_DIST_DIR` in Vercel env vars can cause deploy finalization failures
+  // after `next build` completes (builder can't find expected output).
+  distDir: process.env.VERCEL ? '.next' : (process.env.NEXT_DIST_DIR || '.next'),
   basePath: '',
   images: {
     remotePatterns: [
